98720e617de7df6b03a093246d3f57565ccdc5d8
Walid Mokhtari <khodirmokhtari2@gmail.com>
2025-05-04 19:36:34 +0100

    Add files via upload

---
+++ Space Admins/SpaceAdmins.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace ticket_call_system
+{
+    partial class SpaceAdmins
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnXA1 = new System.Windows.Forms.Button();
+            this.btnXA2 = new System.Windows.Forms.Button();
+            this.btnXB1 = new System.Windows.Forms.Button();
+            this.btnXB2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbTicketNumberA1 = new System.Windows.Forms.Label();
+            this.label6lbTicketNumberA2 = new System.Windows.Forms.Label();
+            this.lbTicketNumberB1 = new System.Windows.Forms.Label();
+            this.lbTicketNumberB2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnXA1
+            // 
+            this.btnXA1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXA1.Location = new System.Drawing.Point(37, 122);
+            this.btnXA1.Name = "btnXA1";
+            this.btnXA1.Size = new System.Drawing.Size(162, 57);
+            this.btnXA1.TabIndex = 0;
+            this.btnXA1.Tag = "0";
+            this.btnXA1.Text = "Next for A1";
+            this.btnXA1.UseVisualStyleBackColor = true;
+            this.btnXA1.Click += new System.EventHandler(this.ClickOnNextButton);
+            // 
+            // btnXA2
+            // 
+            this.btnXA2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXA2.Location = new System.Drawing.Point(233, 122);
+            this.btnXA2.Name = "btnXA2";
+            this.btnXA2.Size = new System.Drawing.Size(162, 57);
+            this.btnXA2.TabIndex = 1;
+            this.btnXA2.Tag = "1";
+            this.btnXA2.Text = "Next for A2";
+            this.btnXA2.UseVisualStyleBackColor = true;
+            this.btnXA2.Click += new System.EventHandler(this.ClickOnNextButton);
+            // 
+            // btnXB1
+            // 
+            this.btnXB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXB1.Location = new System.Drawing.Point(429, 122);
+            this.btnXB1.Name = "btnXB1";
+            this.btnXB1.Size = new System.Drawing.Size(162, 57);
+            this.btnXB1.TabIndex = 2;
+            this.btnXB1.Tag = "2";
+            this.btnXB1.Text = "Next for B1";
+            this.btnXB1.UseVisualStyleBackColor = true;
+            this.btnXB1.Click += new System.EventHandler(this.ClickOnNextButton);
+            // 
+            // btnXB2
+            // 
+            this.btnXB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXB2.Location = new System.Drawing.Point(625, 122);
+            this.btnXB2.Name = "btnXB2";
+            this.btnXB2.Size = new System.Drawing.Size(162, 57);
+            this.btnXB2.TabIndex = 3;
+            this.btnXB2.Tag = "3";
+            this.btnXB2.Text = "Next for B2";
+            this.btnXB2.UseVisualStyleBackColor = true;
+            this.btnXB2.Click += new System.EventHandler(this.ClickOnNextButton);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SteelBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 46);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "A1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(276, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 46);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "A2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(467, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 46);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "B1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.SeaGreen;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(667, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 46);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "B2";
+            // 
+            // lbTicketNumberA1
+            // 
+            this.lbTicketNumberA1.AutoSize = true;
+            this.lbTicketNumberA1.BackColor = System.Drawing.Color.SteelBlue;
+            this.lbTicketNumberA1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTicketNumberA1.Location = new System.Drawing.Point(80, 73);
+            this.lbTicketNumberA1.Name = "lbTicketNumberA1";
+            this.lbTicketNumberA1.Size = new System.Drawing.Size(43, 46);
+            this.lbTicketNumberA1.TabIndex = 8;
+            this.lbTicketNumberA1.Tag = "0";
+            this.lbTicketNumberA1.Text = "1";
+            // 
+            // label6lbTicketNumberA2
+            // 
+            this.label6lbTicketNumberA2.AutoSize = true;
+            this.label6lbTicketNumberA2.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.label6lbTicketNumberA2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6lbTicketNumberA2.Location = new System.Drawing.Point(276, 73);
+            this.label6lbTicketNumberA2.Name = "label6lbTicketNumberA2";
+            this.label6lbTicketNumberA2.Size = new System.Drawing.Size(43, 46);
+            this.label6lbTicketNumberA2.TabIndex = 9;
+            this.label6lbTicketNumberA2.Tag = "1";
+            this.label6lbTicketNumberA2.Text = "1";
+            // 
+            // lbTicketNumberB1
+            // 
+            this.lbTicketNumberB1.AutoSize = true;
+            this.lbTicketNumberB1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.lbTicketNumberB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTicketNumberB1.Location = new System.Drawing.Point(467, 73);
+            this.lbTicketNumberB1.Name = "lbTicketNumberB1";
+            this.lbTicketNumberB1.Size = new System.Drawing.Size(43, 46);
+            this.lbTicketNumberB1.TabIndex = 10;
+            this.lbTicketNumberB1.Tag = "2";
+            this.lbTicketNumberB1.Text = "1";
+            // 
+            // lbTicketNumberB2
+            // 
+            this.lbTicketNumberB2.AutoSize = true;
+            this.lbTicketNumberB2.BackColor = System.Drawing.Color.SeaGreen;
+            this.lbTicketNumberB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTicketNumberB2.Location = new System.Drawing.Point(667, 73);
+            this.lbTicketNumberB2.Name = "lbTicketNumberB2";
+            this.lbTicketNumberB2.Size = new System.Drawing.Size(43, 46);
+            this.lbTicketNumberB2.TabIndex = 11;
+            this.lbTicketNumberB2.Tag = "3";
+            this.lbTicketNumberB2.Text = "1";
+            // 
+            // SpaceAdmins
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.ClientSize = new System.Drawing.Size(811, 191);
+            this.Controls.Add(this.lbTicketNumberB2);
+            this.Controls.Add(this.lbTicketNumberB1);
+            this.Controls.Add(this.label6lbTicketNumberA2);
+            this.Controls.Add(this.lbTicketNumberA1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnXB2);
+            this.Controls.Add(this.btnXB1);
+            this.Controls.Add(this.btnXA2);
+            this.Controls.Add(this.btnXA1);
+            this.Name = "SpaceAdmins";
+            this.Text = "SpaceAdmins";
+            this.Load += new System.EventHandler(this.SpaceAdmins_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnXA1;
+        private System.Windows.Forms.Button btnXA2;
+        private System.Windows.Forms.Button btnXB1;
+        private System.Windows.Forms.Button btnXB2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbTicketNumberA1;
+        private System.Windows.Forms.Label label6lbTicketNumberA2;
+        private System.Windows.Forms.Label lbTicketNumberB1;
+        private System.Windows.Forms.Label lbTicketNumberB2;
+    }
+}
